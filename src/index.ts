59ef5e62a66778eede3d46a37d672559ed37b142
Arnaud Tournier <ltearno@gmail.com>
2017-10-15 09:47:43 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,27 @@ import * as Block from './block'
 import * as Node from './node'
 import * as NodeImpl from './node-impl'
 
-console.log(`creation du block`)
-let block = Block.createBlock(null, [{ nom: "arnaud" }])
-
-console.log(`minage du block`)
-let minedBlock = Block.mineBlock(block, 1001)
-
-console.log(`MINED BLOCK : ${JSON.stringify(minedBlock)}`)
-
+console.log(`creating a node`)
 let node = new NodeImpl.NodeImpl()
-node.addEventListener('head', () => console.log(`new head !`))
+node.addEventListener('head', () => console.log(`event : node has new head (${node.currentBlockChainHead()})`))
+
 console.log(`current head: ${node.currentBlockChainHead()}`)
-let metadata = node.registerBlock(minedBlock)
-console.log(`added block: ${JSON.stringify(metadata)}`)
-console.log(`current head: ${node.currentBlockChainHead()}`)
+
+let nbToMine = 5
+let previousBlock = null
+
+while (nbToMine-- >= 0) {
+    console.log(`block creation`)
+    let block = Block.createBlock(null, [{ nom: "arnaud" }])
+    console.log(`mining block`)
+    let minedBlock = Block.mineBlock(block, 1001)
+    console.log(`mined : ${JSON.stringify(minedBlock)}`)
+
+    console.log(`adding block to node`)
+    let metadata = node.registerBlock(minedBlock)
+    console.log(`added block: ${JSON.stringify(metadata)}`)
+
+    console.log(`current head: ${node.currentBlockChainHead()}`)
+
+    previousBlock = block
+}
