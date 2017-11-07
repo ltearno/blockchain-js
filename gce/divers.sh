54ac047c79367a298befd6aa2d720d20456b1440
Arnaud Tournier <ltearno@gmail.com>
2017-11-07 15:40:54 +0100

    gce

--- gce/divers.sh
+++ gce/divers.sh
@@ -5,7 +5,7 @@ gcloud compute instances create my-app-instance --tags http-server --metadata-fr
 gcloud compute instances get-serial-port-output my-app-instance
 
 # Accéder à la VM en ssh
-gcloud compute --project "blockchain-185216" ssh --zone "europe-west1-c" "my-app-instance"
+gcloud compute --project "blockchain-185216" ssh "my-app-instance"
 
 # Création du firewall pour donner accès à la VM depuis internet
 gcloud compute firewall-rules create default-allow-http-9091 \
@@ -13,3 +13,9 @@ gcloud compute firewall-rules create default-allow-http-9091 \
     --source-ranges 0.0.0.0/0 \
     --target-tags http-server \
     --description "Allow port 9091 access to http-server"
+
+# detruire l'instance
+gcloud compute instances delete "my-app-instance"
+
+# détruire la règle du firewall
+gcloud compute firewall-rules delete default-allow-http-9091
