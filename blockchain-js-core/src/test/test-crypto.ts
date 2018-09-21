796fb587543f4b769e2268bc2b4cc4c99f7990a3
Arnaud Tournier <ltearno@gmail.com>
2018-09-21 18:13:44 +0200

    refactored test crypto

--- blockchain-js-core/src/test/test-crypto.ts
+++ blockchain-js-core/src/test/test-crypto.ts
@@ -1,73 +1,58 @@
+import * as OrderedJson from '../ordered-json'
 import * as HashTools from '../hash-tools'
 
 import * as forge from 'node-forge'
 
-async function test() {
-    var rsa = forge.pki.rsa
-    var keypair = rsa.generateKeyPair({ bits: 2048, e: 0x10001 })
-    console.log(`keypair: ${keypair.privateKey} ${keypair.publicKey}`)
-    console.log('publickey pem: ', forge.pki.publicKeyToPem(keypair.publicKey))
-    console.log('privatekey pem: ', forge.pki.privateKeyToPem(keypair.privateKey))
-    rsa.generateKeyPair({ bits: 2048, workers: 2 }, function (err, keypair) {
-        console.log(`keypair: ${keypair.privateKey} ${keypair.publicKey}`)
-    });
-
-    const secret = `k${Math.random()}`
-    let msg: any = { msg: 'salut', num: 5 }
-    for (let i = 0; i < 100; i++)
-        //msg['m' + i] = 'lkjhlkjhlkjhlkjhlkjhlk jkh lkzje hlzkj helkjz hlezkjh lkjh'
-        msg['m' + i] = 'helloéé^嗨，先生dzaldlhazkj dlazk嗨，先生jdh lzadg zaksj azhgkazjhgnkzejfhg zekfjh gfzjhg' + i * 2
-
+function testAes(msg, secret) {
     let encryptedAes = HashTools.encryptAes(msg, secret)
     let decryptedAes = HashTools.decryptAes(encryptedAes, secret)
-    if (!HashTools.sameObjects(msg, decryptedAes)) {
-        console.error(`error in AES 1`)
-        return
-    }
+    if (!HashTools.sameObjects(msg, decryptedAes))
+        throw `error in AES 1`
+}
 
-    /*encryptedAes = HashTools.encryptAesEx(msg, secret)
-    decryptedAes = HashTools.decryptAesEx(encryptedAes, secret)
-    if (!HashTools.sameObjects(msg, decryptedAes)) {
-        console.error(`error in AES 1`)
-        return
-    }*/
+function testAesEx(msg, secret) {
+    let encryptedAes = HashTools.encryptAesEx(msg, secret)
+    let decryptedAes = HashTools.decryptAesEx(encryptedAes, secret)
+    if (!HashTools.sameObjects(msg, decryptedAes))
+        throw `error in AES 1`
+}
 
-    encryptedAes = HashTools.encryptAes(HashTools.encryptAes(msg, secret), secret)
-    decryptedAes = HashTools.decryptAes(HashTools.decryptAes(encryptedAes, secret), secret)
-    if (!HashTools.sameObjects(msg, decryptedAes)) {
-        console.error(`error in AES 2`)
-        return
-    }
+function testAesDouble(msg, secret) {
+    let encryptedAes = HashTools.encryptAes(HashTools.encryptAes(msg, secret), secret)
+    let decryptedAes = HashTools.decryptAes(HashTools.decryptAes(encryptedAes, secret), secret)
+    if (!HashTools.sameObjects(msg, decryptedAes))
+        throw `error in AES 2`
+}
 
-    const { publicKey, privateKey } = await HashTools.generateRsaKeyPair()
-
-    console.log(`public ${publicKey}`)
-    console.log(`private ${privateKey}`)
-
-
-
-
-
+function testSign(msg, privateKey, publicKey) {
     let signature = HashTools.sign(msg, privateKey)
     console.log(`signature : ${signature}`)
-    let verified = HashTools.verify(msg, signature, publicKey)
-    console.log(`verified (should be true) : ${verified}`)
-    msg.msg = 'altered'
-    verified = HashTools.verify(msg, signature, publicKey)
-    console.log(`verified (should be false) : ${verified}`)
+    if (!HashTools.verify(msg, signature, publicKey))
+        throw `verified (should be true)`
 
+    msg.msg = 'altered'
+
+    if (HashTools.verify(msg, signature, publicKey))
+        throw `verified(should be false)`
+}
+
+function testSignAndPack(msg, privateKey) {
     let signedData = HashTools.signAndPackData(msg, privateKey)
     let isVerified = HashTools.verifyPackedData(signedData)
     let hisSignature = HashTools.extractPackedDataSignature(signedData)
     let hisPublicKey = HashTools.extractPackedDataPublicKey(signedData)
-    console.log(`checked : ${isVerified} ${hisSignature} ${hisPublicKey}`)
 
+    if (!(isVerified && hisSignature && hisPublicKey))
+        throw `failed testSignAndPack: ${isVerified} ${hisSignature} ${hisPublicKey} `
+}
+
+function testSignAndPack2(privateKey, publicKey) {
     let programPayload = {
         version: 0,
         code: 'blablabla',
         ownerPublicKey: publicKey
     }
-    signature = HashTools.sign(programPayload, privateKey)
+    let signature = HashTools.sign(programPayload, privateKey)
 
     let programV2 = {
         version: 1,
@@ -80,17 +65,35 @@ async function test() {
 
     let isFromOwner = HashTools.verify(programPayload, signature, programPayload.ownerPublicKey)
     let isFromOwner2 = HashTools.verify(programV2, sig2, programPayload.ownerPublicKey)
+}
 
-
-
-    //msg = 'hello !'
-    console.log(`clear: ${JSON.stringify(msg)}`)
+function testHybrid(msg, privateKey, publicKey) {
     let encryptedRsa = HashTools.encryptHybrid(msg, publicKey)
     let decryptedRsa = HashTools.decryptHybrid(encryptedRsa, privateKey)
-    console.log(`decrypted: ${JSON.stringify(decryptedRsa)}`)
+    if (OrderedJson.stringify(msg) != OrderedJson.stringify(decryptedRsa))
+        throw `failed hybrid encrypt/decrypt !`
+}
 
-    //let encryptedRsaP = HashTools.encryptRsaPrivate(msg, privateKey)
-    //let decryptedRsaP = HashTools.decryptRsaPublic(encryptedRsaP, publicKey)
+async function test() {
+    let msg: any = { msg: 'salut', num: 5 }
+    for (let i = 0; i < 100; i++)
+        //msg['m' + i] = 'lkjhlkjhlkjhlkjhlkjhlk jkh lkzje hlzkj helkjz hlezkjh lkjh'
+        msg['m' + i] = 'helloéé^嗨，先生dzaldlhazkj dlazk嗨，先生jdh lzadg zaksj azhgkazjhgnkzejfhg zekfjh gfzjhg' + i * 2
+
+    const secret = `k${Math.random()} `
+    console.log(`secrect: ${secret} `)
+
+    const { publicKey, privateKey } = await HashTools.generateRsaKeyPair()
+    console.log(`public ${publicKey} `)
+    console.log(`private ${privateKey} `)
+
+    testAes(msg, secret)
+    //testAesEx(msg,secret)
+    testAesDouble(msg, secret)
+    testSign(msg, privateKey, publicKey)
+    testSignAndPack(msg, privateKey)
+    testSignAndPack2(privateKey, publicKey)
+    testHybrid(msg, privateKey, publicKey)
 
     console.log(`ended tests`)
 }
