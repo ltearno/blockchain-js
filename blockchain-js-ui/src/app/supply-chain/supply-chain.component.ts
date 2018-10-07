d07ed162db15d6049ffba0b6397e92718e5ef66a
Arnaud Tournier <ltearno@gmail.com>
2018-10-07 15:49:48 +0200

    drawing a bit

--- blockchain-js-ui/src/app/supply-chain/supply-chain.component.ts
+++ blockchain-js-ui/src/app/supply-chain/supply-chain.component.ts
@@ -42,14 +42,13 @@ interface GroupWork {
     title: string
     description: string
     author: string
-    lotSize: { width: number; height: number }
+    zoneSize: { width: number; height: number }
     size: { width: number; height: number }
     grid: {
         workItemId: string // id de l'item `groupwork-XXX`, `artwork-XXX`, `pixel-XXX`, `emoji-XXX`
         ownerId: string // le possesseur
         accepted: boolean // contrat accepté avec le possesseur, pour celui-ci, l'objet disparait de son inventaire
     }[] // by line
-    validated: boolean
 }
 
 function findGroupWorkCompatibleAvailableWorkItems() {
@@ -82,24 +81,77 @@ function acceptProposal() {
 const WIDTH = 400
 const HEIGHT = 400
 
-function drawArtWork(artWork: ArtWork, ctx: CanvasRenderingContext2D) {
-    let CW = WIDTH / artWork.size.width
-    let CH = HEIGHT / artWork.size.height
+function drawPixel(color: string, width: number, height: number, ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = color
+    ctx.fillRect(0, 0, width, height)
+}
+
+function drawEmoji(text: string, width: number, height: number, ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = 'black'
+    ctx.font = `${Math.min(width, height) * .64}px Verdana`
+
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(text, width / 2, 1.1 * height / 2)
+}
+
+function drawArtWork(state: ProgramState, artWorkId: string, width: number, height: number, ctx: CanvasRenderingContext2D) {
+    const artWork = state.artWorks[artWorkId]
+
+    let CW = width / artWork.size.width
+    let CH = height / artWork.size.height
 
     for (let i = 0; i < artWork.size.width; i++) {
         for (let j = 0; j < artWork.size.height; j++) {
             let value = artWork.grid[j * artWork.size.width + i]
             if (value) {
-                ctx.fillStyle = value
-                ctx.fillRect(i * CW, j * CH, CW, CH)
-
-                //ctx.textAlign = 'center'
-                ctx.textBaseline = 'top'
-                ctx.fillStyle = "black"
-                ctx.font = `${CW}px Verdana`
-                ctx.fillText("😁", i * CW, i * CH)
+                ctx.save()
+                ctx.translate(i * CW, j * CH)
+                drawWorkItem(state, value, CW, CH, ctx)
+                ctx.restore()
             }
         }
+    }
+}
+
+function drawGroupWork(state: ProgramState, groupWorkId: string, width: number, height: number, ctx: CanvasRenderingContext2D) {
+    const groupWork = state.groupWorks[groupWorkId]
+
+    const CW = width / groupWork.size.width
+    const CH = height / groupWork.size.height
+    const PADDING = 1//CW / 20
+
+    ctx.fillStyle = 'lightgrey'
+    ctx.fillRect(0, 0, width, height)
+
+    for (let i = 0; i < groupWork.size.width; i++) {
+        for (let j = 0; j < groupWork.size.height; j++) {
+            ctx.fillStyle = 'white'
+            ctx.fillRect(i * CW + PADDING, j * CH + PADDING, CW - 2 * PADDING, CH - 2 * PADDING)
+
+            let value = groupWork.grid[j * groupWork.size.width + i]
+            if (value) {
+                ctx.save()
+                ctx.translate(i * CW + PADDING, j * CH + PADDING)
+                drawWorkItem(state, value.workItemId, CW - 2 * PADDING, CH - 2 * PADDING, ctx)
+                ctx.restore()
+            }
+        }
+    }
+}
+
+function drawWorkItem(state: ProgramState, id: string, width: number, height: number, ctx: CanvasRenderingContext2D) {
+    if (id.startsWith('pixel-')) {
+        drawPixel(id.substr('pixel-'.length), width, height, ctx)
+    }
+    else if (id.startsWith('emoji-')) {
+        drawEmoji(id.substr('emoji-'.length), width, height, ctx)
+    }
+    else if (id.startsWith('artwork-')) {
+        drawArtWork(state, id.substr('artwork-'.length), width, height, ctx)
+    }
+    else if (id.startsWith('groupwork-')) {
+        drawGroupWork(state, id.substr('groupwork-'.length), width, height, ctx)
     }
 }
 
@@ -118,14 +170,16 @@ export class SupplyChainComponent implements AfterViewInit {
         let canvas = this.canvas.nativeElement
         this.context = canvas.getContext("2d")
 
-        /*let ctx = this.context
-        ctx.clearRect(0, 0, 400, 400)
-        ctx.fillStyle = "#5599aa"
-        ctx.fillRect(0, 0, 200, 200)
-        ctx.fillStyle = "#f35297"
-        ctx.fillRect(200, 200, 200, 200)*/
+        let state = {
+            accounts: {},
+            artWorks: {},
+            groupWorks: {}
+        }
 
-        drawArtWork(this.artWorks[0], this.context)
+        this.artWorks.forEach(a => state.artWorks[a.id] = a)
+        this.groupWorks.forEach(g => state.groupWorks[g.id] = g)
+
+        drawWorkItem(state, `groupwork-${this.groupWorks[0].id}`, WIDTH, HEIGHT, this.context)
     }
 
     userId = 'me'
@@ -156,36 +210,48 @@ export class SupplyChainComponent implements AfterViewInit {
             description: 'Un test',
             size: { width: 3, height: 3 },
             grid: [
-                null, 'red', null,
-                'red', 'white', 'red',
-                null, 'red', null
+                null, 'pixel-red', 'emoji-😁',
+                'pixel-red', 'pixel-white', 'pixel-red',
+                'emoji-😁', 'pixel-red', 'emoji-😂'
             ]
         }
     ]
 
-    groupWorks = [
+    groupWorks: GroupWork[] = [
         {
-            creator: 'me',
-            name: 'Ile paradisiaque',
+            id: 'klkjhf',
+            author: 'me',
+            title: 'Ile paradisiaque',
             description: 'On souhaite créer une ile où il fait bon vivre. Proposez des zones interressantes et variées !',
             zoneSize: { width: 15, height: 15 },
             size: { width: 10, height: 10 },
-            nbAvailableOffers: 3, // should be processed from the data structure
-            lotGrid: [
-                {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-                {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-                {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-                {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+            grid: [
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, { ownerId: 'lolite', workItemId: 'groupwork-swujb', accepted: false }, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, { ownerId: 'lolite', workItemId: 'groupwork-swujb', accepted: false }, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null
             ]
         },
         {
-            creator: 'lolite',
-            name: 'Hopitâl',
+            id: 'swujb',
+            author: 'lolite',
+            title: 'Hopitâl',
             description: 'Un grand H en noir sur blanc, contribuez par des lots noirs.',
             zoneSize: { width: 3, height: 3 },
             size: { width: 5, height: 5 },
-            nbAvailableOffers: 3,
-            lotGrid: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+            grid: [
+                { ownerId: 'lolite', workItemId: 'artwork-oiuyhkjh', accepted: false }, null, null, null, null,
+                null, { ownerId: 'lolite', workItemId: 'artwork-oiuyhkjh', accepted: false }, null, null, null,
+                null, null, { ownerId: 'lolite', workItemId: 'artwork-oiuyhkjh', accepted: false }, null, null,
+                null, null, null, null, { ownerId: 'lolite', workItemId: 'artwork-oiuyhkjh', accepted: false },
+                null, null, null, null, { ownerId: 'lolite', workItemId: 'artwork-oiuyhkjh', accepted: false }
+            ]
         }
     ]
 
