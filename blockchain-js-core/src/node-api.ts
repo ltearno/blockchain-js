be272792d35afeffafe5e86856cf0cf0effa9c5f
Arnaud Tournier <ltearno@gmail.com>
2018-12-07 21:03:42 +0100

    fix

--- blockchain-js-core/src/node-api.ts
+++ blockchain-js-core/src/node-api.ts
@@ -29,10 +29,6 @@ export interface NodeApi {
 
     /**
      * Retrieves the blockchain head block id
-     * 
-     * TODO:
-     * For the moment this represents the 'master' branch shared through consensus accross nodes.
-     * We should support having multiple 'branches' with different level of validity and different sharing scopes.
      */
     blockChainHead(branch: string): Promise<string>
 
