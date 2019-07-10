b4dae14f91ba1ce0f2e36adc2785a3117535fb97
Arnaud Tournier <ltearno@gmail.com>
2019-07-10 21:52:48 +0000

    wip

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -4,14 +4,14 @@ set -e
 
 echo "pushing blockchain-core..."
 cd blockchain-js-core
-docker build . -t blockchain-js-core:latest
+docker build . -t blockchain-js-core:$(git rev-parse HEAD)
 #docker tag blockchain-js-core:latest eu.gcr.io/blockchain-js/blockchain-js-core:latest
 #gcloud docker -- push eu.gcr.io/blockchain-js/blockchain-js-core:latest
 cd ..
 
 echo "pushing UI..."
 cd blockchain-js-ui
-docker build . -t blockchain-js-ui:latest
+docker build . -t blockchain-js-ui:$(git rev-parse HEAD)
 #docker build tag blockchain-js-ui:latest eu.gcr.io/blockchain-js/blockchain-js-ui:latest
 #gcloud docker -- push eu.gcr.io/blockchain-js/blockchain-js-ui:latest
 cd ..
