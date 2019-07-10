aa197b4e0faea735a23d5acb7b378c94ea746a8b
Arnaud Tournier <ltearno@gmail.com>
2019-07-10 21:46:00 +0000

    wip

--- run-locally.sh
+++ run-locally.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-docker run --name blockchain-js-core -d --restart always -p 9091:9091 blockchain-js-core:latest
-docker run --name blockchain-js-ui -d --restart always -p 8081:8080 blockchain-js-ui:latest
+docker stop blockchain-js-core
+docker rm blockchain-js-core
+docker run --name blockchain-js-core -d --restart always -p 9091:9091 blockchain-js-core:$(git rev-parse HEAD)
+
+docker stop blockchain-js-ui
+docker rm blockchain-js-ui
+docker run --name blockchain-js-ui -d --restart always -p 8081:8080 blockchain-js-ui:$(git rev-parse HEAD)
 
 echo "all done"
